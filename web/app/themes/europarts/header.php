36f63c6a66f41a04d3e95e013bd8f2e3bf51afca
Christian <christian.zurli@gmail.com>
2020-11-14 15:46:00 +0100

    14/11/2020

--- web/app/themes/europarts/header.php
+++ web/app/themes/europarts/header.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <?php wp_head() ?>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@200;300;400;600&display=swap" rel="stylesheet">
     <link href="<?php echo get_template_directory_uri() ?>/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() ?>/vendor/slick/slick.css">
     <link rel="stylesheet" type="text/css"
